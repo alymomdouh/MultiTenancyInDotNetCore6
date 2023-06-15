2b724fa780b92a8d203062e512fb45e17dc4493f
alyxmomdouh@gmail.com <alyxmomdouh@gmail.com>
2023-06-15 22:06:26 +0300

    add Code Refactoring for ConfigureServices

---
+++ MultiTenancyInDotNetCore6.API/ConfigureServices.cs
@@ -0,0 +1,30 @@
+﻿namespace MultiTenancyInDotNetCore6.API
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddTenancy(this IServiceCollection services, ConfigurationManager configuration)
+        {
+             services.Configure<TenantSettings>(configuration.GetSection(nameof(TenantSettings)));
+            //Configure DbContext
+            TenantSettings options = new();
+            configuration.GetSection(nameof(TenantSettings)).Bind(options); 
+            var defaultDbProvider = options.Defaults.DBProvider; 
+            if (defaultDbProvider.ToLower() == "mssql")
+            {
+                services.AddDbContext<ApplicationDbContext>(m => m.UseSqlServer());
+            } 
+            foreach (var tenant in options.Tenants)
+            {
+                var connectionString = tenant.ConnectionString ?? options.Defaults.ConnectionString; 
+                using var scope = services.BuildServiceProvider().CreateScope();
+                var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>(); 
+                dbContext.Database.SetConnectionString(connectionString); 
+                if (dbContext.Database.GetPendingMigrations().Any())
+                {
+                    dbContext.Database.Migrate();
+                }
+            } 
+            return services;
+        }
+    }
+}
--- MultiTenancyInDotNetCore6.API/Startup.cs
+++ MultiTenancyInDotNetCore6.API/Startup.cs
@@ -12,33 +12,10 @@ namespace MultiTenancyInDotNetCore6.API
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-            services.Configure<TenantSettings>(Configuration.GetSection(nameof(TenantSettings)));
-            TenantSettings options = new();
-            Configuration.GetSection(nameof(TenantSettings)).Bind(options);
+        { 
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>(); 
             services.AddTransient<ITenantService, TenantService>();
-            services.AddTransient<IProductService, ProductService>();
-
-            //Configure DbContext
-            var defaultDbProvider = options.Defaults.DBProvider; 
-            if (defaultDbProvider.ToLower() == "mssql")
-            {
-                services.AddDbContext<ApplicationDbContext>(m => m.UseSqlServer());
-            } 
-            foreach (var tenant in options.Tenants)
-            {
-                var connectionString = tenant.ConnectionString ?? options.Defaults.ConnectionString; 
-                using var scope = services.BuildServiceProvider().CreateScope();
-                var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>(); 
-                dbContext.Database.SetConnectionString(connectionString); 
-                if (dbContext.Database.GetPendingMigrations().Any())
-                {
-                    dbContext.Database.Migrate();
-                }
-            }
-
+            services.AddTransient<IProductService, ProductService>(); 
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
