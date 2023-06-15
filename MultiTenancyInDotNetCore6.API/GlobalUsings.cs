8378e49c853e7f392afef5a32edb097293b6c249
alyxmomdouh@gmail.com <alyxmomdouh@gmail.com>
2023-06-15 18:16:15 +0300

    add needed nuget Packages for EntityFrameworkCore

--- MultiTenancyInDotNetCore6.API/GlobalUsings.cs
+++ MultiTenancyInDotNetCore6.API/GlobalUsings.cs
@@ -1,5 +1,6 @@
-﻿global using MultiTenancyInDotNetCore6.API.Contracts;
-global using MultiTenancyInDotNetCore6.API.Settings;
+﻿global using Microsoft.EntityFrameworkCore;
 global using Microsoft.OpenApi.Models;
+global using MultiTenancyInDotNetCore6.API.Contracts;
 global using MultiTenancyInDotNetCore6.API.IServices;
 global using MultiTenancyInDotNetCore6.API.Services;
+global using MultiTenancyInDotNetCore6.API.Settings;
